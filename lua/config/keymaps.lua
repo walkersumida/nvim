b349ff47cfad31e49a99536acf62f72d608ea96f
WalkerSumida <walkersumida@gmail.com>
2025-05-10 15:59:24 +0900

    refactor: format lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,63 +2,58 @@ local M = {}
 
 function M.setup()
   -- Save
-  vim.keymap.set('n', '<leader>w', ':w!<CR>')
+  vim.keymap.set("n", "<leader>w", ":w!<CR>")
 
   -- Clear highlight
-  vim.keymap.set('n', '<leader><CR>', ':noh<CR>', { silent = true })
+  vim.keymap.set("n", "<leader><CR>", ":noh<CR>", { silent = true })
 
   -- Visual paste (p)
-  vim.keymap.set('x', 'p', 'pgvy')
+  vim.keymap.set("x", "p", "pgvy")
 
   -- Execute macro @q
-  vim.keymap.set('n', '<C-.>', '@q')
+  vim.keymap.set("n", "<C-.>", "@q")
 
   -- Move visual lines
-  vim.keymap.set('n', 'j', 'gj')
-  vim.keymap.set('n', 'k', 'gk')
-  vim.keymap.set('n', '<Down>', 'gj')
-  vim.keymap.set('n', '<Up>', 'gk')
+  vim.keymap.set({ "n", "v" }, "j", "gj")
+  vim.keymap.set({ "n", "v" }, "k", "gk")
+  vim.keymap.set({ "n", "v" }, "<Down>", "gj")
+  vim.keymap.set({ "n", "v" }, "<Up>", "gk")
 
   -- Terminal keymaps
   local function set_terminal_keymaps()
     local opts = { buffer = 0 }
-    vim.keymap.set('t', '<C-\\>', [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "<C-\\>", [[<C-\><C-n>]], opts)
   end
 
-  vim.api.nvim_create_autocmd('TermOpen', {
-    pattern = 'term://*',
+  vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "term://*",
     callback = set_terminal_keymaps,
   })
 
-  -- Normal mode
-  vim.keymap.set('n', 'sh', ':<C-u>sp<CR>')
-  vim.keymap.set('n', 'sv', ':<C-u>vs<CR>')
-  vim.keymap.set('n', 'x', '"_x')
-  vim.keymap.set('n', 'ZZ', ':qa<CR>')
+  vim.keymap.set("n", "sh", ":<C-u>sp<CR>")
+  vim.keymap.set("n", "sv", ":<C-u>vs<CR>")
+  vim.keymap.set("n", "x", '"_x')
+  vim.keymap.set("n", "ZZ", ":qa<CR>")
 
-  -- Insert mode
-  vim.keymap.set('i', '<C-p>', '<C-c>gka')
-  vim.keymap.set('i', '<C-n>', '<C-c>gja')
-  vim.keymap.set('i', '<C-l>', '<C-o>zz')
-  vim.keymap.set('i', 'jj', '<ESC>')
+  vim.keymap.set("i", "<C-p>", "<C-c>gka")
+  vim.keymap.set("i", "<C-n>", "<C-c>gja")
+  vim.keymap.set("i", "<C-l>", "<C-o>zz")
+  vim.keymap.set("i", "jj", "<ESC>")
 
-  -- Visual mode
-  vim.keymap.set('v', 'x', '"_x')
+  vim.keymap.set("v", "x", '"_x')
 
-  -- Insert + Command-line mode (noremap! 相当)
-  vim.keymap.set({ 'i', 'c' }, '<C-b>', '<Left>')
-  vim.keymap.set({ 'i', 'c' }, '<C-f>', '<Right>')
-  vim.keymap.set({ 'i', 'c' }, '<C-e>', '<End>')
-  vim.keymap.set({ 'i', 'c' }, '<C-a>', '<Home>')
-  vim.keymap.set({ 'i', 'c' }, '<C-d>', '<Del>')
-  vim.keymap.set({ 'i', 'c' }, '<C-k>', '<C-o>D')
-  vim.keymap.set({ 'i', 'c' }, '<C-y>', '<C-r>"')
+  vim.keymap.set({ "i", "c" }, "<C-b>", "<Left>")
+  vim.keymap.set({ "i", "c" }, "<C-f>", "<Right>")
+  vim.keymap.set({ "i", "c" }, "<C-e>", "<End>")
+  vim.keymap.set({ "i", "c" }, "<C-a>", "<Home>")
+  vim.keymap.set({ "i", "c" }, "<C-d>", "<Del>")
+  vim.keymap.set({ "i", "c" }, "<C-k>", "<C-o>D")
+  vim.keymap.set({ "i", "c" }, "<C-y>", '<C-r>"')
 
-  -- Window navigation (map → ノーマルモードで十分)
-  vim.keymap.set('n', '<C-j>', '<C-W>j')
-  vim.keymap.set('n', '<C-k>', '<C-W>k')
-  vim.keymap.set('n', '<C-h>', '<C-W>h')
-  vim.keymap.set('n', '<C-l>', '<C-W>l')
+  vim.keymap.set("n", "<C-j>", "<C-W>j")
+  vim.keymap.set("n", "<C-k>", "<C-W>k")
+  vim.keymap.set("n", "<C-h>", "<C-W>h")
+  vim.keymap.set("n", "<C-l>", "<C-W>l")
 end
 
 return M

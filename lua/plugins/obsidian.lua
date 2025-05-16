044e30ce9ad20ff792a42d7041d88dc9d48cac87
WalkerSumida <walkersumida@gmail.com>
2025-05-16 12:47:58 +0900

    feat: add keymaps for obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -17,6 +17,14 @@ return {
         path = os.getenv("OBSIDIAN_WORKSPACE_PATH") or "",
       },
     },
+    note_id_func = function(title)
+      local date = os.date("%Y-%m-%d")
+      if title ~= nil then
+        return date .. "_" .. title:gsub(" ", "_")
+      else
+        return date
+      end
+    end,
     follow_url_func = function(url)
       vim.fn.jobstart({ "open", url })
     end,
@@ -33,5 +41,18 @@ return {
     require("obsidian").setup(opts)
 
     vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show backlinks" })
+    vim.keymap.set("n", "<leader>ot", function()
+      local tag = vim.fn.input("Tags: ")
+      if tag ~= "" then
+        vim.cmd("ObsidianTags " .. tag)
+      end
+    end, { desc = "Prompt for a tag and run :ObsidianTags [TAG ...]" })
+    vim.keymap.set("n", "<leader>or", "<cmd>ObsidianRename<CR>", { desc = "Rename file" })
+    vim.keymap.set("n", "<leader>oc", function()
+      local name = vim.fn.input("Name: ")
+      if name ~= "" then
+        vim.cmd("ObsidianNew " .. name)
+      end
+    end, { desc = "Prompt for a name and run :ObsidianNew [NAME]" })
   end,
 }

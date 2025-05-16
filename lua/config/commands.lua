20032f989fdc7fc00a61087d4c49f9dc3e427676
WalkerSumida <walkersumida@gmail.com>
2025-05-16 22:05:08 +0900

    feat: add CopyFilePathWithLine cmd & formatting

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,24 +1,31 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_create_user_command('CopyFilePath', function()
-    vim.fn.setreg('*', vim.fn.expand('%:p'))
+  vim.api.nvim_create_user_command("CopyFilePath", function()
+    vim.fn.setreg("*", vim.fn.expand("%:p"))
   end, {})
 
-  vim.api.nvim_create_user_command('CopyPath', function()
-    vim.fn.setreg('*', vim.fn.expand('%:h'))
+  vim.api.nvim_create_user_command("CopyFilePathWithLine", function()
+    local filepath = vim.fn.expand("%:p")
+    local linenum = vim.fn.line(".")
+    local full = string.format("%s:%dL", filepath, linenum)
+    vim.fn.setreg("*", full)
   end, {})
 
-  vim.api.nvim_create_user_command('CopyFileName', function()
-    vim.fn.setreg('*', vim.fn.expand('%:t'))
+  vim.api.nvim_create_user_command("CopyPath", function()
+    vim.fn.setreg("*", vim.fn.expand("%:h"))
   end, {})
 
-  vim.api.nvim_create_user_command('EncodeSJIS', function()
-    vim.cmd('e ++enc=sjis')
+  vim.api.nvim_create_user_command("CopyFileName", function()
+    vim.fn.setreg("*", vim.fn.expand("%:t"))
   end, {})
 
-  vim.api.nvim_create_user_command('SaveWithoutFormatting', function()
-    vim.cmd('noa w')
+  vim.api.nvim_create_user_command("EncodeSJIS", function()
+    vim.cmd("e ++enc=sjis")
+  end, {})
+
+  vim.api.nvim_create_user_command("SaveWithoutFormatting", function()
+    vim.cmd("noa w")
   end, {})
 end
 

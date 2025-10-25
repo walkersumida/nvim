9cb39524ffae92696932e5d86aa222c0751b8b35
WalkerSumida <walkersumida@gmail.com>
2025-10-25 19:33:35 +0900

    feat: change theme of lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 function M.setup()
-  require('lualine').setup {
-    options = { theme = 'onedark' },
+  require("lualine").setup({
+    options = { theme = "solarized_dark" },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'filename' },
-      lualine_c = { 'branch', 'diff', 'diagnostics' },
-      lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' }
+      lualine_a = { "mode" },
+      lualine_b = { "filename" },
+      lualine_c = { "branch", "diff", "diagnostics" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
     },
     inactive_sections = {
       lualine_a = {},
-      lualine_b = { 'filename' },
-      lualine_c = { 'branch', 'diff', 'diagnostics' },
-      lualine_x = { 'location' },
+      lualine_b = { "filename" },
+      lualine_c = { "branch", "diff", "diagnostics" },
+      lualine_x = { "location" },
       lualine_y = {},
-      lualine_z = {}
+      lualine_z = {},
     },
-  }
+  })
 end
 
 return M

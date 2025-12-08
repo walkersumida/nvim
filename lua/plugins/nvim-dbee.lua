fd4c2f56fae621b8abb7164ad8bb6e93a8cd79d8
WalkerSumida <walkersumida@gmail.com>
2025-12-08 14:13:38 +0900

    feat(nvim-dbee): add EnvSource for database connections

--- lua/plugins/nvim-dbee.lua
+++ lua/plugins/nvim-dbee.lua
@@ -8,13 +8,19 @@ return {
       require("dbee").install()
     end,
     config = function()
-      require("dbee").setup()
+      require("dbee").setup({
+        sources = {
+          require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
+        },
+      })
       vim.keymap.set("n", "<leader>ss", function()
         -- Temporarily disable OptionSet events to prevent treesitter fold errors
         local saved = vim.o.eventignore
         vim.o.eventignore = "OptionSet"
         require("dbee").toggle()
-        vim.defer_fn(function() vim.o.eventignore = saved end, 50)
+        vim.defer_fn(function()
+          vim.o.eventignore = saved
+        end, 50)
       end, { desc = "Toggle DBee" })
     end,
   },

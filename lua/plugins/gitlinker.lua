69ffa62f8c614abd1ed8c68b3d9470c4dd0fa93c
WalkerSumida <walkersumida@gmail.com>
2026-05-21 23:27:56 +0900

    feat: add GitLinkCurrentBranch command

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -1,8 +1,14 @@
 return {
   {
     "linrongbin16/gitlinker.nvim",
-    cmd = "GitLink",
+    cmd = { "GitLink", "GitLinkCurrentBranch" },
     opts = {},
     keys = {},
+    config = function()
+      require("gitlinker").setup()
+      vim.api.nvim_create_user_command("GitLinkCurrentBranch", function()
+        require("gitlinker").link({ router_type = "current_branch" })
+      end, { range = true })
+    end,
   },
 }

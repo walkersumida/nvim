ef05a02b86eb3b469c8b177403499279101ee139
WalkerSumida <walkersumida@gmail.com>
2026-08-15 14:01:32 +0900

    feat(gitlinker): support GitHub SSH-over-HTTPS remotes

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -5,7 +5,31 @@ return {
     opts = {},
     keys = {},
     config = function()
-      require("gitlinker").setup()
+      require("gitlinker").setup({
+        router = {
+          browse = {
+            ["^ssh%.github%.com$"] = "https://github.com/"
+              .. "{_A.ORG}/{_A.REPO}/blob/{_A.REV}/"
+              .. "{_A.FILE}?plain=1"
+              .. "#L{_A.LSTART}"
+              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+          },
+          blame = {
+            ["^ssh%.github%.com$"] = "https://github.com/"
+              .. "{_A.ORG}/{_A.REPO}/blame/{_A.REV}/"
+              .. "{_A.FILE}?plain=1"
+              .. "#L{_A.LSTART}"
+              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+          },
+          current_branch = {
+            ["^ssh%.github%.com$"] = "https://github.com/"
+              .. "{_A.ORG}/{_A.REPO}/blob/{_A.CURRENT_BRANCH}/"
+              .. "{_A.FILE}?plain=1"
+              .. "#L{_A.LSTART}"
+              .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+          },
+        },
+      })
       vim.api.nvim_create_user_command("GitLinkCurrentBranch", function()
         require("gitlinker").link({ router_type = "current_branch" })
       end, { range = true })

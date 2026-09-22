4eab9ece1928117c807ce2df9ff4307401774229
WalkerSumida <walkersumida@gmail.com>
2026-09-22 15:52:54 +0900

    feat: update md-table-wrap.nvim plug

--- lua/plugins/md-table-wrap.lua
+++ lua/plugins/md-table-wrap.lua
@@ -3,5 +3,15 @@ return {
     "walkersumida/md-table-wrap.nvim",
     ft = "markdown",
     opts = {},
+    keys = {
+      {
+        "<leader>mt",
+        function()
+          require("md-table-wrap").open_float()
+        end,
+        ft = "markdown",
+        desc = "Open the markdown table in a float",
+      },
+    },
   },
 }
